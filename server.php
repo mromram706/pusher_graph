286c8839518de609a0d0b3fb8d1019d8c8c7badc
mromram706 <131792936+mromram706@users.noreply.github.com>
2024-02-27 16:46:37 +0100

    Update server.php

--- server.php
+++ server.php
@@ -17,5 +17,5 @@ for ($i = 1; $i < 10; $i++) {
         array(rand(100,200), -rand(30, 300), rand(30, 130), rand(80, 120), -rand(10, 200), rand(100, 200), rand(77, 177), -rand(50, 150), rand(60, 160), -rand(10, 80)
         )
     );
-    sleep(1);
+    time_nanosleep(1,0);
 }
